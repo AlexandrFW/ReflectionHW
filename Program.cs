7a7cef34b36584e3b8f9f64371d3c2be9a9fc4ba
Alexandr Kutiev <antizboy1985@gmail.com>
2022-12-16 00:09:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using System.Runtime.Serialization;
+
+Console.WriteLine("Reflection homework");
+
+
+var stopwatch = new Stopwatch();
+
+stopwatch.Start();
+
+
+
+stopwatch.Stop();
+
+
+[Serializable]
+class F : ISerializable
+{ 
+    int i1, i2, i3, i4, i5;
+
+    public void GetObjectData(SerializationInfo info, StreamingContext context)
+    {
+        throw new NotImplementedException();
+    }
+
+    void Get() => new F() { i1 = 1, i2 = 2, i3 = 3, i4 = 4, i5 = 5 }; 
+}
